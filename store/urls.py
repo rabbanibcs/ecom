11023efb5832bad1d15ed453abfe3bd5655c3951
rabbanibcs <rabbanibcs@gmail.com>
2021-04-20 01:28:49 +0600

    Initial commit

---
+++ store/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import cart,checkout, store
+urlpatterns = [
+    path('store/', store,name='store'),
+    path('cart/', cart,name='cart'),
+    path('checkout/', checkout,name='checkout'),
+]
