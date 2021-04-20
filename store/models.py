be2420223e25e2abb697183eaf9cbce7de4de2c2
rabbanibcs <rabbanibcs@gmail.com>
2021-04-21 04:28:19 +0600

    shipping Address model added.

--- store/models.py
+++ store/models.py
@@ -12,7 +12,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price=models.FloatField()
     digital=models.BooleanField(default=False)
-    #image=
+    image=models.ImageField(blank=True,upload_to='images',default='images/placeholder.png')
+
     def __str__(self):
         return self.name
 
@@ -24,9 +25,22 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
-class CartItems(models.Model):
+class CartItem(models.Model):
     product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True) #
-    order=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
     quantity=models.IntegerField(default=0,null=True,blank=True)
     date_added=models.DateField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
+    address=models.CharField(max_length=200, null=True)
+    city=models.CharField(max_length=200, null=True)
+    state=models.CharField(max_length=200, null=True)
+    zipcode=models.CharField(max_length=200, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
 
