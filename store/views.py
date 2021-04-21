dd4c7e220f89a445a3130f27d6d79452b583dd1d
rabbanibcs <rabbanibcs@gmail.com>
2021-04-22 01:19:17 +0600

    Cart view added

--- store/views.py
+++ store/views.py
@@ -7,7 +7,15 @@ def store(request):
     return render(request,'store/store.html',context)
 
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order=Order.objects.get(customer=customer)
+        items=order.cartitem_set.all()
+    else:
+        items=[]
+
+    # total_price=float(items.quantity)*float(items.price)
+    context={'items':items}
     return render(request,'store/cart.html',context)
     
 def checkout(request):
