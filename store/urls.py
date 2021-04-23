79094687555a208557d16332f4914c94388adfd8
rabbanibcs <rabbanibcs@gmail.com>
2021-04-24 02:14:44 +0600

    class views applied.

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import cart,checkout, store
+from .views import Product_View,StoreView,CartView,Checkout
 urlpatterns = [
-    path('store/', store,name='store'),
-    path('cart/', cart,name='cart'),
-    path('checkout/', checkout,name='checkout'),
+    path('store/', StoreView.as_view(),name='store'),
+    path('cart/', CartView.as_view(),name='cart'),
+    path('checkout/', Checkout.as_view(),name='checkout'),
+    path('detail/<int:pk>/', Product_View.as_view(), name='detail'),
 ]
