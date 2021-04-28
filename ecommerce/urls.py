e565e865bd97a3e34efaf5a2c6a974baba1e0132
rabbanibcs <rabbanibcs@gmail.com>
2021-04-28 22:30:34 +0600

    login , logout , register rout added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('store.urls'))
+    path('',include('store.urls')),
+    path('eshop/', include('user.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
